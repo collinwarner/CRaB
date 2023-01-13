b1c67b2931a8c7f16ca63a5f790496e7044840ba
Carlos Castillo <carloscaslo19@gmail.com>
2023-01-13 16:04:26 -0500

    modified cmake and build files for envin the cluster

--- cpp_skeleton/build.sh
+++ cpp_skeleton/build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DBOOST_ROOT=/home/software/boost/1.70.0 -D CMAKE_C_COMPILER=/home/software/gcc/8.3.0/bin/gcc -DCMAKE_BUILD_TYPE=Debug ..
 make
 cd ..
