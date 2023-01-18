7d5014487a905aa32b3ed05f70bcb5db8d4b42ad
Carlos Castillo <carloscaslo19@gmail.com>
2023-01-18 12:49:30 -0500

    remove modules at start of setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-module load engaging/git/2.4.0-rc3
+#!/bin/bash
+
+# Remove previous modules
+module purge
+
+# Load required modules for pokerbots
 module load anaconda3/2021.11
 source activate workspace 
 module load cmake/3.17.3
 module load boost/1.70.0
 module load gcc/8.3.0
+module load engaging/git/2.4.0-rc3
